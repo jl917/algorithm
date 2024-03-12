8f034ef47d8965d7737f4b53eae281e058692efd
jl917 <julong1988@naver.com>
2024-03-12 19:31:42 +0900

    day39 등록

---
+++ leetcode/39_145_stack.js
@@ -0,0 +1,46 @@
+const { TreeNode } = require("./tree_node");
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+var postorderTraversal = function (root) {
+  if (!root) {
+    return [];
+  }
+  const result = [];
+  const stack = [root];
+
+  while (stack.length) {
+    const currentItem = stack.pop();
+    result.unshift(currentItem.val);
+    currentItem.left && stack.push(currentItem.left);
+    currentItem.right && stack.push(currentItem.right);
+  }
+
+  return result;
+};
+
+// const t1 = new TreeNode(1);
+// const t2 = new TreeNode(2);
+// const t3 = new TreeNode(3);
+
+// t1.right = t2;
+// t2.left = t3;
+
+const t5 = new TreeNode(5);
+const t4 = new TreeNode(4);
+const t6 = new TreeNode(6);
+const t1 = new TreeNode(1);
+const t2 = new TreeNode(2);
+const t7 = new TreeNode(7);
+const t8 = new TreeNode(8);
+
+t5.left = t4;
+t5.right = t6;
+t4.left = t1;
+t4.right = t2;
+t6.left = t7;
+t6.right = t8;
+
+console.log(postorderTraversal(t5));
---
+++ leetcode/39_94_stack.js
@@ -0,0 +1,51 @@
+const { TreeNode } = require("./tree_node");
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+var inorderTraversal = function (root) {
+  if(!root){
+    return [];
+  }
+  const result = [];
+  const stack = [];
+  let currentItem = root;
+
+  while (stack.length || currentItem) {
+    if (currentItem) {
+      stack.push(currentItem);
+      currentItem = currentItem.left;
+    } else {
+      currentItem = stack.pop();
+      result.push(currentItem.val);
+      currentItem = currentItem.right;
+    }
+  }
+
+  return result;
+};
+
+// const t1 = new TreeNode(1);
+// const t2 = new TreeNode(2);
+// const t3 = new TreeNode(3);
+
+// t1.right = t2;
+// t2.left = t3;
+
+const t5 = new TreeNode(5);
+const t4 = new TreeNode(4);
+const t6 = new TreeNode(6);
+const t1 = new TreeNode(1);
+const t2 = new TreeNode(2);
+const t7 = new TreeNode(7);
+const t8 = new TreeNode(8);
+
+t5.left = t4;
+t5.right = t6;
+t4.left = t1;
+t4.right = t2;
+t6.left = t7;
+t6.right = t8;
+
+console.log(inorderTraversal(t5));

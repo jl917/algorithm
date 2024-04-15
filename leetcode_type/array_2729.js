d9359724997ca1496d6bc862676f8ffdbda996e0
jl917 <julong1988@naver.com>
2024-04-15 22:09:48 +0900

    성능 좋은 버전으로

--- leetcode_type/array_2729.js
+++ leetcode_type/array_2729.js
@@ -6,19 +6,36 @@ var isFascinating = function (n) {
   if (n > 333) {
     return false;
   }
+
   const set = new Set();
 
-  for(let i = 1; i< 4; i++){
-    tmp = n * i;
-    while (tmp > 0) {
-      if (set.has(tmp % 10) || tmp % 10 === 0) {
-        return false;
-      }
-      set.add(tmp % 10);
-      tmp = Math.floor(tmp / 10);
+  let tmp = n;
+
+  while (tmp > 0) {
+    if (set.has(tmp % 10) || tmp % 10 === 0) {
+      return false;
     }
+    set.add(tmp % 10);
+    tmp = Math.floor(tmp / 10);
   }
 
+  tmp = n * 2;
+  while (tmp > 0) {
+    if (set.has(tmp % 10) || tmp % 10 === 0) {
+      return false;
+    }
+    set.add(tmp % 10);
+    tmp = Math.floor(tmp / 10);
+  }
+
+  tmp = n * 3;
+  while (tmp > 0) {
+    if (set.has(tmp % 10) || tmp % 10 === 0) {
+      return false;
+    }
+    set.add(tmp % 10);
+    tmp = Math.floor(tmp / 10);
+  }
   return true;
 };
 console.log(isFascinating(192));
